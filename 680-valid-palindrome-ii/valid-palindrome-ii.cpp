890478b7227abcb59522f3567940785134b3ef93
Garvit Trivedi <garvit.trivedi.cg@gmail.com>
2026-04-16 16:48:10 +0530

    Time: 11 ms (11.29%) | Memory: 26.6 MB (40.21%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+
+    bool check(string s, int i,int j){
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+               i++;
+                j--;
+        }
+        return true;
+    }
+public:
+    bool validPalindrome(string s) {
+int n = s.size();
+int i=0;
+int j =n-1;
+while(i<j){
+    if(s[i]!=s[j]){
+        return check(s,i+1,j) || check(s,i,j-1);
+    }
+    i++;
+    j--;
+}
+return true;
+
+    }
+};
