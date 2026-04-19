4db5ed2840d8a614f8964172044f2a9f3da001cb
Garvit Trivedi <garvit.trivedi.cg@gmail.com>
2026-04-19 23:54:34 +0530

    Time: 6 ms (66.28%) | Memory: 65.6 MB (11.15%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+      var  rem,temp ,final=0;
+    var number=x;
+    temp=number;
+    while(number>0){
+        rem=number%10;
+        number=parseInt(number/10);
+        final=final*10+rem;
+    }
+    if(final==temp){
+        return true;
+    }
+    else{
+        return 0;
+    } 
+};
