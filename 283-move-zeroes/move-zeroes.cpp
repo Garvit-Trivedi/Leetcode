a2adec662604fe74a3f4187c95e7e91177731f04
Garvit Trivedi <garvit.trivedi.cg@gmail.com>
2026-05-23 10:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (98.50%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-     int a =0;
-     for(int i=0;i<nums.size();i++){
-        if(nums[i]!=0){
-            swap(nums[i],nums[a]);
-            a++;
-        }
-     }
+    int n = nums.size();
+    int k = 0;
+    for(int i=0;i<n;i++){
+if(nums[i]!=0){
+    swap(nums[i],nums[k]);
+    k++;
+}
+    }
         
     }
 };
