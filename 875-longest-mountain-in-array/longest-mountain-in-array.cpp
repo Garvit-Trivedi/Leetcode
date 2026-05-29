007386dab6d65ff20a025ddd1d65337bbe2c1e2c
Garvit Trivedi <garvit.trivedi.cg@gmail.com>
2026-05-29 18:09:11 +0530

    Time: 1 ms (25.04%) | Memory: 22.3 MB (54.16%) - LeetSync

---
+++ 875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        int n = arr.size();
+if(n<3) return 0;
+int ans = 0;
+for(int i =1;i<n-1;i++){
+    if(arr[i] > arr[i-1] && arr[i] >arr[i+1]){
+        int l=i;
+        int r=i;
+        while(l>0 && arr[l] >arr[l-1]){
+            l--;
+        }
+        while(r<n-1 && arr[r] >arr[r+1]){
+            r++;
+        } 
+            ans = max(ans, r-l+1);
+    } 
+
+}
+
+return ans;
+    }
+};
