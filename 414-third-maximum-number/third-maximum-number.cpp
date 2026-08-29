e55e8a6cee599622f1ab5fdb3c45f6049ffeee7c
Garvit Trivedi <garvit.trivedi.cg@gmail.com>
2026-08-29 11:06:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (93.65%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-     long max1 = LONG_MIN;
-      long max2 = LONG_MIN;
-       long max3 = LONG_MIN;
+long long  f= LLONG_MIN;
+long long s = LLONG_MIN;
+long long t = LLONG_MIN;
+for(int x : nums){
+    if( x == f || x == s || x == t)
+    continue;
 
-       for(int arr : nums){
-        if(arr == max1 || arr == max2 || arr == max3 )
-            continue;
-        
-        if(arr>max1){
-            max3 = max2;
-            max2 = max1;
-            max1 = arr;
-        } 
-       else if(arr > max2){
-            max3 = max2;
-            max2 = arr;
-        }
-      else  if(arr>max3){
-            max3= arr;
-        }
-       }
-       return max3 == LONG_MIN?max1 : max3;
+    if(x>f){
+        t=s;
+        s=f;
+        f=x;
     }
+
+ else if(x>s){
+        t = s;
+        s = x;
+    }
+
+    else if(x>t){
+        t = x;
+    }
+
+   
+} 
+
+if(t == LLONG_MIN){
+return f;
+}
+
+return t;
+}
 };
